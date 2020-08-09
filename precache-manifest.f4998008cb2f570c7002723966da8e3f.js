0acc666de800f4123c2ee3eb74dae00bbeb8a804
Joe Hernandez <yoshi.jhii@gmail.com>
2020-08-09 12:15:54 -0500

    Updates

---
+++ precache-manifest.f4998008cb2f570c7002723966da8e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc97b6e9977b08cb3e4481d3b262e91",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "62b24459009da2b22793",
+    "url": "/reactPortfolio/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "8e08d88349028c68313e",
+    "url": "/reactPortfolio/static/js/2.1659cbd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.1659cbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b24459009da2b22793",
+    "url": "/reactPortfolio/static/js/main.bf9155ad.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  }
+]);
